a3c0c0e59795e34477d3c4053cad3e27a3cbe9af
Vihang Supare <vihang7660@gmail.com>
2022-03-16 12:10:11 +0530

    Added clear button, fixed dimensions of the container grid, given flex properties.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let container = document.createElement('div')
 let body = document.querySelector('body')
+let button = document.createElement('button')
+button.textContent = 'Clear'
+body.appendChild(button)
+let container = document.createElement('div')
 body.appendChild(container)
 container.className = 'container'
 
@@ -11,20 +14,23 @@ let child;
 
 for (let i = 0; i < rows; i++) {
     parent = document.createElement('div')
+    parent.style.display = 'flex'
+    parent.style.flex = '1'
+    parent.style.flexDirection = 'column'
     container.appendChild(parent)
     parent.className = 'parent'
     for (let k = 0; k < column; k++) {
         child = document.createElement('div')
         parent.appendChild(child)
         child.className = 'child'
-        child.style.border = '1px solid red'
-        child.style.width = '20px'
-        child.style.height = '20px'
+        child.style.flex = '1'
     }
 }
 
+body.style.display = 'flex'
+body.style.flexDirection = 'column'
+body.style.alignItems = 'center'
 container.style.display = 'flex'
-container.style.justifyContent = 'center'
 
 function hover(e) {
     e.currentTarget.style.backgroundColor = 'green'
@@ -36,3 +42,19 @@ let boxes = Array.from(box)
 for (let i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener('mouseover', hover)
 }
+
+function clear() {
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].style.backgroundColor = ''
+    }
+}
+
+button.addEventListener('click', clear)
+button.style.margin = '10px'
+button.style.height = '30px'
+button.style.width = '100px'
+
+container.style.display = 'flex'
+container.style.height = '400px'
+container.style.width = '400px'
+container.style.border = '1px solid black'
